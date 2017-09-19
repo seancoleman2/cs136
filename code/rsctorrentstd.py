f41e6b2d277a39573aea7116da1f1664cfd09312
seancoleman2 <seancoleman@college.harvard.edu>
2017-09-19 00:03:02 -0400

    BitTorrent reference client implemented. Need to revisit tomorrow to confirm it is working as designed.

--- code/rsctorrentstd.py
+++ code/rsctorrentstd.py
@@ -8,16 +8,19 @@
 
 import random
 import logging
+import numpy as np 
+import pandas as pd
 
 from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
-class RSCTorrent(Peer):
+class RSCTorrentStd(Peer):
     def post_init(self):
         print "post_init(): %s here!" % self.id
-        self.dummy_state = dict()
-        self.dummy_state["cake"] = "lie"
+        self.state = dict()
+        self.state["round"] = 0
+        self.state["optimistic_spot"] = None
     
     def requests(self, peers, history):
         """
@@ -53,21 +56,24 @@ class RSCTorrent(Peer):
         peers.sort(key=lambda p: p.id)
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
+        counts = pd.DataFrame(data=np.zeros(len(self.pieces)))
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
-            n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
+            for piece_index in isect:
+                counts.iloc[piece_index, 0] = counts.iloc[piece_index, 0] + 1
 
+
+        filtered_counts = counts[counts.index.isin(np_set)]
+        sorted_counts = filtered_counts.sort_values(0)
+        n = min(self.max_requests, sorted_counts.size)
+
+        for piece_id in sorted_counts.index.values[:n]:
+             for peer in peers:
+                if piece_id in peer.available_pieces:     
+                    start_block = self.pieces[piece_id]
+                    r = Request(self.id, peer.id, piece_id, start_block)
+                    requests.append(r)
         return requests
 
     def uploads(self, requests, peers, history):
@@ -84,10 +90,6 @@ class RSCTorrent(Peer):
         round = history.current_round()
         logging.debug("%s again.  It's round %d." % (
             self.id, round))
-        # One could look at other stuff in the history too here.
-        # For example, history.downloads[round-1] (if round != 0, of course)
-        # has a list of Download objects for each Download to this peer in
-        # the previous round.
 
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
@@ -95,16 +97,51 @@ class RSCTorrent(Peer):
             bws = []
         else:
             logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
 
-            request = random.choice(requests)
-            chosen = [request.requester_id]
+            # HISTORY: check previous 2 rounds 
+            prev_two_round_downloads = history.downloads[-2:]
+            peer_blocks_downloaded = pd.DataFrame(data=np.zeros(len(peers)), index=[peer.id for peer in peers])
+            for round in prev_two_round_downloads:
+                for download in round:
+                    peer_blocks_downloaded.loc[download.from_id] = peer_blocks_downloaded.loc[download.from_id] + download.blocks
+
+            # RECIPROCATION: compute top 3 uploaders who also requested
+            sorted_peers = peer_blocks_downloaded.sort_values(0)
+            sorted_peers_with_pos_upload = sorted_peers[sorted_peers[0] > 0].index.values
+            requesters = [request.requester_id for request in requests]
+            sorted_requesters_with_pos_upload = set(sorted_peers_with_pos_upload).intersection(set(requesters))
+
+            # find top 3 and list of possible candidates for optimistic unchoking
+            top_3_requesters = list(sorted_requesters_with_pos_upload)[:3]
+            other_requesters = [requester for requester in requesters if requester not in top_3_requesters]
+
+            # OPTIMISTIC UNCHOKING: unchoke randomly every 3 stages
+            if len(other_requesters) > 0:
+                if self.state["optimistic_spot"] is not None:
+                    if self.state["round"] % 3 == 0:            
+                        optimistic_spot = [random.choice(other_requesters)]
+                    else:
+                        optimistic_spot = [self.state["optimistic_spot"]]
+                else:
+                    optimistic_spot = [random.choice(other_requesters)]
+
+                self.state["optimistic_spot"] = optimistic_spot[0]
+
+            else:
+                optimistic_spot = []
+
+            chosen = top_3_requesters + optimistic_spot
+
             # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+            if(len(chosen) > 0):
+                bws = even_split(self.up_bw, len(chosen))
+            else:
+                bws = []
 
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
+
+        self.state["round"] += 1
             
         return uploads
