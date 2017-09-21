8675b9a2c3b764147dcec32a9184fceb55bc3834
seancoleman2 <seancoleman@college.harvard.edu>
2017-09-20 20:31:04 -0400

    BitTyrany CAPPED version implemented

--- code/rsctorrenttyrant.py
+++ code/rsctorrenttyrant.py
@@ -18,8 +18,8 @@ import pandas as pd
 class RSCTorrentTyrant(Peer):
     def post_init(self):
         print "post_init(): %s here!" % self.id
-        self.state = dict()
 
+        self.state = dict()
         # to adjust flow in and expected upload rate to attain unchoking
         self.state["conseq_rounds_unchoked_by"] = dict()
 
@@ -42,8 +42,8 @@ class RSCTorrentTyrant(Peer):
         """
         needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
         needed_pieces = filter(needed, range(len(self.pieces)))
-        np_set = set(needed_pieces)  # sets support fast intersection ops.
 
+        np_set = set(needed_pieces)
 
         logging.debug("%s here: still need pieces %s" % (
             self.id, needed_pieces))
@@ -56,33 +56,37 @@ class RSCTorrentTyrant(Peer):
         logging.debug("look at the AgentHistory class in history.py for details")
         logging.debug(str(history))
 
-        requests = []   # We'll put all the things we want here
-        # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
-        
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
-        # request all available pieces from all peers!
-        # (up to self.max_requests from each)
+        requests = [] 
+
+        # COMPUTE RARITY - number of instances of a given piece
         counts = pd.DataFrame(data=np.zeros(len(self.pieces)))
-        for peer in peers:
-            av_set = set(peer.available_pieces)
-            isect = av_set.intersection(np_set)
-            for piece_index in isect:
+        for peer in peers:  
+            for piece_index in set(peer.available_pieces):
                 counts.iloc[piece_index, 0] = counts.iloc[piece_index, 0] + 1
 
+        # RAREST FIRST - request the pieces held by the fewest people
+        # shuffle counts to break symmetry, then sort
+        counts = counts.sample(frac=1)
+        sorted_counts = counts.sort_values(0)
 
-        filtered_counts = counts[counts.index.isin(np_set)]
-        sorted_counts = filtered_counts.sort_values(0)
-        n = min(self.max_requests, sorted_counts.size)
+        # filter to only request elements we need
+        filtered_sorted_counts = sorted_counts[sorted_counts.index.isin(np_set)]
+        max_pieces_to_download = min(self.max_requests, filtered_sorted_counts.size)
 
-        for piece_id in sorted_counts.index.values[:n]:
-             for peer in peers:
+        # iterate peers, requesting pieces in order of rarity
+        for peer in peers:
+            pieces_downloaded_from_peer = 0
+            for piece_id in filtered_sorted_counts.index.values:
                 if piece_id in peer.available_pieces:     
                     start_block = self.pieces[piece_id]
                     r = Request(self.id, peer.id, piece_id, start_block)
                     requests.append(r)
+
+                    # check that we do not exceed maximum possible downloads
+                    pieces_downloaded_from_peer = pieces_downloaded_from_peer + 1
+                    if pieces_downloaded_from_peer == max_pieces_to_download:
+                        break
+
         return requests
 
     def uploads(self, requests, peers, history):
@@ -96,7 +100,9 @@ class RSCTorrentTyrant(Peer):
         In each round, this will be called after requests().
         """
 
-        # initially assume 4 upload spots
+        ### CAPPED Version of Tyrant because we have Max Bandwidth ## 
+
+        # initially assume 4 upload spots, with same bw_cap 
         init_spots, bw_cap, = 4, self.up_bw
         initializer = float(even_split(bw_cap, init_spots)[0])
 
@@ -120,6 +126,7 @@ class RSCTorrentTyrant(Peer):
                 piece_delta = len(peer.available_pieces) - self.state["previously_available_pieces"][peer.id]
                 self.state["blocks_downloaded_last_round"][peer.id] = piece_delta*self.conf.blocks_per_piece
 
+            # update previously available pieces at "end of round" - this will not be adjusted after this
             self.state["previously_available_pieces"][peer.id] = len(peer.available_pieces)
 
         if len(requests) == 0:
@@ -127,7 +134,7 @@ class RSCTorrentTyrant(Peer):
             chosen = []
             bws = []
         else:
-            
+
             if round != 0:
                 prev_round_downloads = history.downloads[-1:]
                 prev_round_unchokers = []
@@ -180,9 +187,6 @@ class RSCTorrentTyrant(Peer):
                     if total_t_j < bw_cap:
                         chosen.append(peer)
                         bws.append(float(self.state["expected_upload_rates"][peer]))
-
-            if len(chosen) == 0:
-                bws = []
 
 
         # create actual uploads out of the list of peer ids and bandwidths
