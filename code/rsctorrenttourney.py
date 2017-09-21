70df46b322a89c9d47f9ec8788643531bee33b90
Robert Shaw <robertgshaw2@gmail.com>
2017-09-21 10:37:09 -0400

    finished auditing tyrant file

--- code/rsctorrenttourney.py
+++ code/rsctorrenttourney.py
@@ -8,16 +8,31 @@
 
 import random
 import logging
+import math
+import numpy as np 
+import pandas as pd
 
 from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
-class RSCTorrent(Peer):
+
+class RSCTorrentTyrant(Peer):
     def post_init(self):
         print "post_init(): %s here!" % self.id
-        self.dummy_state = dict()
-        self.dummy_state["cake"] = "lie"
+
+        self.state = dict()
+
+        # to adjust flow in and expected upload rate to attain unchoking
+        self.state["conseq_rounds_unchoked_by"] = dict()
+
+        # to conlude a peers download rate
+        self.state["previously_available_pieces"] = dict()
+        self.state["blocks_downloaded_last_round"] = dict() # represents total download_in 
+
+        # used for ratios
+        self.state["exp_down_bw"] = dict()
+        self.state["exp_min_up_bw"] = dict()    
     
     def requests(self, peers, history):
         """
@@ -30,8 +45,8 @@ class RSCTorrent(Peer):
         """
         needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
         needed_pieces = filter(needed, range(len(self.pieces)))
-        np_set = set(needed_pieces)  # sets support fast intersection ops.
 
+        np_set = set(needed_pieces)
 
         logging.debug("%s here: still need pieces %s" % (
             self.id, needed_pieces))
@@ -44,29 +59,36 @@ class RSCTorrent(Peer):
         logging.debug("look at the AgentHistory class in history.py for details")
         logging.debug(str(history))
 
-        requests = []   # We'll put all the things we want here
-        # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
-        
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
-        # request all available pieces from all peers!
-        # (up to self.max_requests from each)
+        requests = [] 
+
+        # COMPUTE RARITY - number of instances of a given piece
+        counts = pd.DataFrame(data=np.zeros(len(self.pieces)))
+        for peer in peers:  
+            for piece_index in set(peer.available_pieces):
+                counts.iloc[piece_index, 0] = counts.iloc[piece_index, 0] + 1
+
+        # RAREST FIRST - request the pieces held by the fewest people
+        # shuffle counts to break symmetry, then sort
+        counts = counts.sample(frac=1)
+        sorted_counts = counts.sort_values(0)
+
+        # filter to only request elements we need
+        filtered_sorted_counts = sorted_counts[sorted_counts.index.isin(np_set)]
+        max_pieces_to_download = min(self.max_requests, filtered_sorted_counts.size)
+
+        # iterate peers, requesting pieces in order of rarity
         for peer in peers:
-            av_set = set(peer.available_pieces)
-            isect = av_set.intersection(np_set)
-            n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
+            pieces_downloaded_from_peer = 0
+            for piece_id in filtered_sorted_counts.index.values:
+                if piece_id in peer.available_pieces:     
+                    start_block = self.pieces[piece_id]
+                    r = Request(self.id, peer.id, piece_id, start_block)
+                    requests.append(r)
+
+                    # check that we do not exceed maximum possible downloads
+                    pieces_downloaded_from_peer = pieces_downloaded_from_peer + 1
+                    if pieces_downloaded_from_peer == max_pieces_to_download:
+                        break
 
         return requests
 
@@ -81,30 +103,107 @@ class RSCTorrent(Peer):
         In each round, this will be called after requests().
         """
 
+        ### CAPPED Version of Tyrant because we have Max Bandwidth ## 
+
+        # initially assume 4 upload spots, with same bw_cap 
+        init_spots, bw_cap, = 4, self.up_bw
+        initializer = float(even_split(bw_cap, init_spots)[0])
+
         round = history.current_round()
         logging.debug("%s again.  It's round %d." % (
             self.id, round))
-        # One could look at other stuff in the history too here.
-        # For example, history.downloads[round-1] (if round != 0, of course)
-        # has a list of Download objects for each Download to this peer in
-        # the previous round.
+
+        # used for calculating the download rate 
+        for peer in peers:
+            if round == 0:
+                # initialize dictionaries
+                self.state["blocks_downloaded_last_round"][peer.id] = 0
+                self.state["conseq_rounds_unchoked_by"][peer.id] = 0
+
+                # we are estimating the other peers have the same number 
+                # of upload spots and bw in the first round
+                self.state["exp_down_bw"][peer.id] = initializer
+                self.state["exp_min_up_bw"][peer.id] = initializer
+
+            else: 
+                piece_delta = len(peer.available_pieces) - self.state["previously_available_pieces"][peer.id]
+                self.state["blocks_downloaded_last_round"][peer.id] = piece_delta * self.conf.blocks_per_piece
+
+            # update previously available pieces at "end of round" - this will not be adjusted after this
+            self.state["previously_available_pieces"][peer.id] = len(peer.available_pieces)
 
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
             chosen = []
             bws = []
         else:
-            logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
+            if round != 0:
+                prev_round_downloads = history.downloads[-1:]
+                prev_round_unchokers = set([])
 
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+                # compute how many blocks you have downloaded form each peer j last round
+                blocks_downloaded_last_round = pd.DataFrame(data=np.zeros(len(peers)), index=[peer.id for peer in peers])
+                for round in prev_round_downloads:
+                    for download in round: 
+                        blocks_downloaded_last_round.loc[download.from_id] += download.blocks
+                        prev_round_unchokers.add(download.from_id)
+
+                # if currently unchoked, expected download rate is the number of blocks you downloaded
+                for prev_round_unchoker in prev_round_unchokers:
+                    self.state["exp_down_bw"][prev_round_unchoker] = blocks_downloaded_last_round.loc[prev_round_unchoker].values[0]
+                    self.state["conseq_rounds_unchoked_by"][prev_round_unchoker] += 1 
+
+                    # if you have been unchoked for more than 2 rounds, decrease your expected required minimum upload rate for reciprocation
+                    if self.state["conseq_rounds_unchoked_by"][prev_round_unchoker] > 2: 
+                        u_j = self.state["exp_min_up_bw"][prev_round_unchoker]
+                        gamma = .1
+                        sigma = .5
+                        self.state["exp_min_up_bw"][prev_round_unchoker] = (1 - gamma - sigma**round) * u_j
+
+                # those not unchoking us
+                prev_round_chokers = [x for x in [peer.id for peer in peers] if x not in prev_round_unchokers]
+                for prev_round_choker in prev_round_chokers:
+                    # expect flow to be download_in/4
+                    self.state["exp_down_bw"][prev_round_choker] = self.state["blocks_downloaded_last_round"][prev_round_choker] / float(init_spots)
+
+                    # increase T_j for those who are choking us
+                    alpha = .2
+                    sigma = .5
+                    self.state["exp_min_up_bw"][prev_round_choker] = (1 + alpha + sigma**round) * self.state["exp_min_up_bw"][prev_round_choker]
+                    self.state["conseq_rounds_unchoked_by"][prev_round_choker] = 0
+
+            # ratios
+            ratios = dict()
+            f_ji, t_j = self.state["exp_down_bw"], self.state["exp_min_up_bw"]
+            for peer in peers:
+                # if they have been doing no downloading, do not share
+                if t_j[peer.id] == 0: 
+                    ratios[peer.id] = 0.0
+                # otherwise, look at the return on investment
+                else:
+                    ratios[peer.id] = float(f_ji[peer.id])/float(t_j[peer.id])
+
+            sorted_ratios = sorted(ratios, key=ratios.get, reverse=True)
+
+            # iterate requesters in order or sorted ratios
+            requesters = [request.requester_id for request in requests]
+            chosen, bws = [], [] 
+            total_t_j = 0.0
+            for peer in sorted_ratios: 
+                if peer in requesters:
+                    # round up -> going below expected min threshold will waste BW
+                    peer_exp_min_up_bw = math.ceil(self.state["exp_min_up_bw"][peer])
+
+                    # check to see we don't exceed the upload capacity
+                    if peer_exp_min_up_bw + total_t_j < bw_cap:
+                        chosen.append(peer)
+                        bws.append(peer_exp_min_up_bw)
+
+                        # increment
+                        total_t_j += peer_exp_min_up_bw
 
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
-            
+
         return uploads
